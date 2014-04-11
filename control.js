c4e9ff9289e285fdd2e4d6f30b986f8a1ac9a1d1
laanak <laanak@gmail.com>
2014-04-11 11:51:32 -0400

    multi select for nonconsecutive elements

---
+++ control.js
@@ -0,0 +1,60 @@
+attach_multiselect_detection_events();
+
+// FIXME: change signiture to accept opts/selects as params
+function attach_multiselect_detection_events(elems){
+	window.formSelects = {};
+
+	var opts = document.getElementsByTagName('option');
+	var selects = document.getElementsByTagName('select');
+
+
+	var give_focus = function(){ this.focus(); };
+
+	var handle_ctrl_keypress = function(event){
+		var ctrlKeyCode = 17;
+		if( event.keyCode === ctrlKeyCode){
+			if( event.type === 'keydown'){
+				formSelects.ctrlKey = true;
+			}else if( event.type === 'keyup'){
+				formSelects.ctrlKey = false;
+			}
+		}
+	};
+
+	var add_remove_selected = function(){
+		var child = this;
+		var parentID = this.parentNode.id;
+		var val = child.value;
+
+		if( !formSelects[parentID]){
+			formSelects[parentID] = { selected : {} };
+			formSelects[parentID].selected[val] = 1;
+		} else {
+			if( formSelects.ctrlKey === true ){
+				if( exists(val,formSelects[parentID].selected) ){
+					delete formSelects[parentID].selected[val];
+				}
+			} else if( !formSelects.ctrlKey || formSelects.ctrlKey === false ){
+				delete formSelects[parentID];
+				formSelects[parentID] = { selected : {} };
+				formSelects[parentID].selected[val] = 1;
+			}
+
+		}
+	};
+
+	var exists = function(item,dataset){
+		return (dataset[item] ? true : false );
+	};
+
+	for(var i = 0, numOpts = opts.length; i < numOpts; ++i){
+		opts[i].onclick = add_remove_selected;
+	}
+
+	for(var j = 0, numSelects = selects.length; j < numSelects; ++j){
+		selects[j].onmouseover = give_focus;
+		selects[j].onkeydown = handle_ctrl_keypress;
+		selects[j].onkeyup = handle_ctrl_keypress;
+	}
+}
+
